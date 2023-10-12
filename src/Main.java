00c6b29dfbfd950703619fc2c66d0981ce35919b
TilekovAmanjan <Amanenrirj@gmail.com>
2023-10-12 21:05:26 +0600

    hw

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         BankAccount account = new BankAccount(15000.0);
+
         try {
             while (true) {
                 account.withDraw(6000.0);
